cf5f79eb4234e2eb5b6de43782f85ed011b874a4
Martin Andreev <martin_i_a@abv.bg>
2024-09-19 01:02:39 +0300

    Setup the search form functionality. Setup default values for the search fields
    Now the query params are passed in the URL when a search is done.

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -2,6 +2,7 @@
 
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import {
   Select,
@@ -13,11 +14,23 @@ import {
 import { JobStatus } from "@/utils/types";
 
 function SearchForm() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const router = useRouter();
+  const pathname = usePathname();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const search = formData.get("search") as string;
     const jobStatus = formData.get("jobStatus") as string;
+
+    const params = new URLSearchParams();
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   return (
@@ -25,8 +38,13 @@ function SearchForm() {
       className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3  gap-4 rounded-lg"
       onSubmit={handleSubmit}
     >
-      <Input type="text" placeholder="Search Jobs" name="search" />
-      <Select name="jobStatus">
+      <Input
+        type="text"
+        placeholder="Search Jobs"
+        name="search"
+        defaultValue={search}
+      />
+      <Select defaultValue={jobStatus} name="jobStatus">
         <SelectTrigger>
           <SelectValue />
         </SelectTrigger>
