057528039203c615eb130c831481baf3cd83e6d0
Martin Andreev <martin_i_a@abv.bg>
2024-09-19 20:40:28 +0300

    Implement delete job functionality
    We started in actions.ts and continued in DeleteJobBtn.tsx.
    Now the delete button in the job badge is working successfully.

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -40,7 +40,7 @@ function JobCard({ job }: { job: JobType }) {
         <Button asChild size="sm">
           <Link href={`/jobs/${job.id}`}>edit</Link>
         </Button>
-        <DeleteJobBtn />
+        <DeleteJobBtn id={job.id} />
       </CardFooter>
     </Card>
   );
